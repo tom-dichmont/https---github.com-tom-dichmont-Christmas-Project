b07ff50192443bd6445dfa157b551bcaf5679dc6
tomdichmont <t.dichmont@gmail.com>
2022-01-05 20:27:55 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+      id: 1,
+      name: "Coach House",
+      type: "Residential",
+      technologies: "Steel frame, reinforced concrete, masonry",
+      methodologies: "BIM, Agile",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo."              
+    },
+    {
+      id: 2,
+      name: "Landscaping Scheme",
+      type: "Residential",
+      technologies: "Reinforced concrete, masonry, Cor-Ten Steel",
+      methodologies: "BIM, Agile",
+      description: "AAA"
+    },
+    {
+      id: 3,
+      name: "Pavillion",
+      type: "Residential",
+      technologies: "Steel frame, Cor-Ten Steel",
+      methodologies: "BIM, Agile",
+      description: "AAA"
+    },
+  ];
